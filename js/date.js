006a6199428deda37fd2b7c8b2bbb82b9a0f863e
Moises R Sanchez <59586957+MoisesSanchez2020@users.noreply.github.com>
2020-01-11 21:30:19 -0800

    Add files via upload

--- js/date.js
+++ js/date.js
@@ -1,4 +1,6 @@
 
-const options={weekday:'long', day: 'numeric', month: 'long', year:'numeric'};
+const options={hour: 'numeric', minute: 'numeric',weekday:'long', day: 'numeric', month: 'long', year:'numeric'};
 document.getElementById('upDate').textContent=new Date().toLocaleDateString('en-US', options);
+
+
 
