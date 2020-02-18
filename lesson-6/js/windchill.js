7588bcefb1ca9d1c8492aa5fdb86cb826e904c76
Moises R Sanchez <59586957+MoisesSanchez2020@users.noreply.github.com>
2020-02-17 17:25:48 -0700

    Add files via upload

--- lesson-6/js/windchill.js
+++ lesson-6/js/windchill.js
@@ -1,34 +1,23 @@
 
 function windChill() {
-    let windSpeed = document.getElementById('windSp').innerHTML;
-    windSpeed = parseFloat(windSpeed);
-    let temperature = document.getElementById('temp').innerHTML;
-    temperature = parseFloat(temperature);
-    if (temperature <= 50 && windSpeed >= 3) {
-        let windspeedExponent = Math.pow(windSpeed, 0.16);
-        let windChill = (35.74 + 0.6215 * temperature - 35.75 * windspeedExponent + 0.4275 * temperature * windspeedExponent).toFixed(0);
+    var windForce = document.getElementById('windSp').innerHTML;
+    windForce = parseFloat(windForce);
+    
+    var frioTemp = document.getElementById('temp').innerHTML;
+    frioTemp = parseFloat(frioTemp);
+
+    if (frioTemp<= 50 && windForce >= 3) {
+        var tempWindForce = Math.pow(windForce, 0.16);
+        var windChill = (35.74 + 0.6215 * frioTemp - 35.75 * tempWindForce + 0.4275 * frioTemp * tempWindForce).toFixed(0);
         return windChill;
     } else {
-        return temperature;
+        return frioTemp;
     }
 
 }
-// Wind Chill calculation
-document.getElementById("displayWind").innerHTML = windChill() + "&#xb0;F";
+
+document.getElementById("displayTable").innerHTML = windChill() + "&#xb0;F";
 
 
 
 
-
-//
-// "Wind Chill Temperature is officially defined for temperatures at or below 10 °C (50 °F) and 
-// wind speeds above 4.8 kilometers per hour (3.0 mph)."
-//
-/*let temp = parseInt(document.getElementById("temperature").innerText);
-let windspeed = parseInt(document.getElementById("windspeed").innerText);
-if(temp <= 50 && windspeed >= 3) {
-    let mathstuff = 35.74 + (0.6215*temp) - (35.75*(windspeed**0.16)) + (0.4275*temp*(windspeed**0.16));
-    document.getElementById("windchill").innerText = mathstuff.toFixed(2);
-} else {
-   document.getElementById("windchill").innerText = "N/A" 
-}*/
